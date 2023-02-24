2ab8df973d3bd33e86b040851954f47ebea95c0a
Chaz Kondo <chuckeekondo@gmail.com>
2023-02-24 05:59:54 -1000

    not enough time

--- count-vowels-permutation/count-vowels-permutation.js
+++ count-vowels-permutation/count-vowels-permutation.js
@@ -116,7 +116,7 @@ var countVowelPermutationTDR = function(n) {
 };
 
 // Top Down Reversed Staircase Revised - AC
-var countVowelPermutationTDRR = function(n) {
+var countVowelPermutation = function(n) {
     
     const memo = {}
     function dfs(cl, ld) {
@@ -146,58 +146,61 @@ var countVowelPermutationTDRR = function(n) {
 };
 
 
-
-// Bottom Up DP, 
+// Bottom Up DP,
 var countVowelPermutation = function(n) {
-    
+
     // it seems like a 2 dimensional dp based on my previous memostr
-    // my base case for the dp will be when the current length is 0 
+    // my base case for the dp will be when the current length is 0
     // (revised to 0 from 1)
-    
+
     // so in the first example, we have n = 0 (revised to 0)
     // it looks like we would have a loop that goes through
     // each of the five letters, and aggregates the amounts
-    
+
     // dp might look something like
     // [[1], [1], [1], [1], [1]]
     // this would sum up to 5
-    
+
     // in the second example, n = 2
     // and we should get an output of 10
-    
+
     // dp might be initialized to
     // [[1, 0], [1, 0], [1, 0], [1, 0], [1, 0]]
-    
+
     // the first loop would iterate through the arrays
     // the second loop would iterate through the letters
     // in the previous example where we only had the base case,
     // the second loop may not have run due to there being no room in dp
+
+    // so dp is generally reflecting current length ->
+    // meaning, at each index, go through the possible outcomes
+    // and count them up
     
-    // -> return here
+    // there for we must initialize the dp to have an inner array
+    // for each of the possible starting numbers
+    // and the inner for loop will just help us count as we go on
     
-    const memo = {}
-    function dfs(cl, ld) {
-        if (cl === 0) return 1
-        
-        const memostr = `${cl}, ${ld}`
-        if (memostr in memo) return memo[memostr]
-        
-        if (ld === 0) memo[memostr] = dfs(cl - 1, 1)
-        if (ld === 1) memo[memostr] = dfs(cl - 1, 0) + dfs(cl - 1, 2)
-        if (ld === 2) {
-            memo[memostr] = 0
-            for (let i = 0; i < 5; i++) 
-                if (i !== 2) memo[memostr] += dfs(cl - 1, i)
+    // something is still wrong with my logic here.
+    // spent time on codespaces so I didn't have enough time here
+    // I'll come back tomorrow
+
+    const dp = Array(n).fill().map(() => Array(6).fill(0))
+    dp.map(arr => arr[0] = 1)
+
+    for (let i = 0; i < n; i++) {
+        for (let j = 1; j < 6; j++) {
+            if (j === 0) dp[i][j] = dp[i - 1][1]
+            if (j === 1) dp[i][j] = dp[i - 1][0] + dp[i - 1][2]
+            if (j === 2) {
+                for (let k = 0; i < 5; k++)
+                    if (k !== 2) dp[i][j] += dp[i - 1][k]
+            }
+            if (j === 3) dp[i][j] = dp[i - 1][2] + dp[i - 1][4]
+            if (j === 4) dp[i][j] = dp[i - 1][0]
+
+            dp[i][j] = dp[i][j] % (10 ** 9 + 7)
         }
-        if (ld === 3) memo[memostr] = dfs(cl - 1, 2) + dfs(cl - 1, 4)
-        if (ld === 4) memo[memostr] = dfs(cl - 1, 0)
-        
-        return memo[memostr] % (10 ** 9 + 7)
     }
     
-    let ans = 0
-    for (let i = 0; i < 5; i++) {
-        ans += dfs(n - 1, i)
-    }
-    return ans % (10 ** 9 + 7)
+    console.log(dp, 'sanity')
 };
