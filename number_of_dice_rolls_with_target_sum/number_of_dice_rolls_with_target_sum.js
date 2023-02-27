d594075355f9863723a1316d287b8b16bef40517
Chaz Kondo <chuckeekondo@gmail.com>
2023-02-27 05:54:26 -1000

    attempt to do a brute force. not working as wanted

---
+++ number_of_dice_rolls_with_target_sum/number_of_dice_rolls_with_target_sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @param {number} target
+ * @return {number}
+ */
+var numRollsToTarget = function(n, FACES, target) {
+    if (n === 1) return target <= FACES ? 1 : 0 // base case
+    
+    let dp = Array(FACES + 1).fill(0)
+    for (let i = 0; i < dp.length; i++) {
+        dp[i] = i
+    }
+    
+    let total = 0
+    
+    for (let i = 1; i < n; i++) {
+        for (let j = 1; j < FACES + 1; j++) {
+            for (let k = 1; k < FACES + 1; k++) {
+                if (k + dp[j] === target) total++
+            }
+        }
+    }
+    
+    return total
+    
+    
+};
