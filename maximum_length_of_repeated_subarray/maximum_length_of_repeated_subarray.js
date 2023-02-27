d3063950d7a74d9b03b854039de0bb4e3f7acaf8
Chaz Kondo <chuckeekondo@gmail.com>
2023-02-27 05:37:43 -1000

    tried brute force and it worked

--- maximum_length_of_repeated_subarray/maximum_length_of_repeated_subarray.js
+++ maximum_length_of_repeated_subarray/maximum_length_of_repeated_subarray.js
@@ -3,7 +3,9 @@
  * @param {number[]} nums2
  * @return {number}
  */
-var findLength = function(n1, n2) {
+
+// WA
+var findLengthTD = function(n1, n2) {
     
     const memo = {}
     
@@ -19,4 +21,33 @@ var findLength = function(n1, n2) {
     }
     
     return dfs(0, 0)
+};
+
+
+// try to do Brute force and it worked.
+
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findLength = function(n1, n2) {
+    
+    let b = 0 // best
+    for (let i = 0; i < n1.length; i++) {
+        for (let j = 0; j < n2.length; j++) {
+            if (n1[i] === n2[j]) {
+                let c = 1
+                for (let k = 1; k < Math.min(n1.length - i, n2.length - j); k++) {
+                    if (n1[k + i] === n2[k + j]) {
+                        c++
+                    } else {
+                        break
+                    }
+                }
+                b = Math.max(c, b)
+            }
+        }
+    }
+    return b
 };
