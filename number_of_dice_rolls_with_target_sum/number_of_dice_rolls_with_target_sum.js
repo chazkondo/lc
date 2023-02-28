e788e68a4c0d9d2a8ff8662da1ecf6102f25d6ee
Chaz Kondo <chuckeekondo@gmail.com>
2023-02-28 05:44:16 -1000

    try top down

--- number_of_dice_rolls_with_target_sum/number_of_dice_rolls_with_target_sum.js
+++ number_of_dice_rolls_with_target_sum/number_of_dice_rolls_with_target_sum.js
@@ -15,10 +15,15 @@
 // create a dp that either has all levels of n
 // or update dp in a way that reflects the last sums
 
-// * may need to approach for a top down brute force to 
+// * may need to approach from a top down brute force to 
 // fully understand the complexity of the problem
 
-var numRollsToTarget = function(n, FACES, target) {
+// [7-1=6, 7-2=5, 7-3=4, 7-4=3, 7-5=2, 7-6=1]
+
+// it seems that on each level down, i'll need to keep track of k times k more states 
+// to iterate through. 
+
+var numRollsToTargetAttemptToDP = function(n, FACES, target) {
     if (n === 1) return target <= FACES ? 1 : 0 // base case
     
     let dp = Array(FACES + 1).fill(0)
@@ -38,6 +43,12 @@ var numRollsToTarget = function(n, FACES, target) {
     }
     
     return total
+    
+    
+};
+
+var numRollsToTarget = function(n, FACES, target) {
+    // try top down
     
     
 };
