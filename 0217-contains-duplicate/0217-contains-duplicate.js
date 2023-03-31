415116770401ea43a163033b4440dbdea232be1f
chuckee kondo <38027158+chazkondo@users.noreply.github.com>
2023-03-31 12:05:16 -1000

    Time: 84 ms (92.03%), Space: 55.3 MB (27.18%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.js
+++ 0217-contains-duplicate/0217-contains-duplicate.js
@@ -2,17 +2,4 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var containsDuplicate = function(nums) {
-    const mark = Array(150000);
-    for (let i of nums) {
-        let j = i/8;
-        let k = i%8;
-        let check = 1<<k;
-        if ((mark[j] & check) != 0) {
-            return true;
-        }
-        mark[j] |= check;
-    }
-    return false;
-    
-};
+var containsDuplicate = nums => new Set(nums).size !== nums.length
